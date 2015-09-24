d5442c81353bbba21da3f40db81eb8c3212dd1d3
ShivMOD <ricohet@op.pl>
2015-09-25 00:33:27 +0200

    asd

--- Braum.lua
+++ Braum.lua
@@ -70,8 +70,8 @@ if CanUseSpell(myHero, _W) and BraumMenu.Combo.sbm:Value() and GoS:GetDistance(m
 local spacepress = KeyIsDown(0x20) --F7 OrbWalker is ON
 if spacepress then
 local target = GetCurrentTarget()
-local Qprediction = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1150,300,1000,80,true,true)
-local Rprediction = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),750,1000,1250,120,false,true)
+local Qprediction = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1400,300,1000,80,true,true)
+local Rprediction = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1600,300,1250,120,false,true)
       
 if CanUseSpell(myHero, _Q) == READY and BraumMenu.Combo.Q:Value() and GoS:ValidTarget(target, 1000) and Qprediction.HitChance == 1 then
 CastSkillShot(_Q,Qprediction.PredPos.x, Qprediction.PredPos.y, Qprediction.PredPos.z)
